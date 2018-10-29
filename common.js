1e42ed609c8ddbad0b546f8c2575bcf0a2a1d11c
Inna Shingaryova <i.shingaryova@lovata.com>
2018-10-29 17:12:31 +0300

    Add top navbar

--- common.js
+++ common.js
@@ -1,1 +1,12 @@
-console.log('work!');
+window.$ = window.jQuery = require('jquery');
+require ('popper.js');
+require ('bootstrap');
+
+$(function() { // Shorthand for $( document ).ready()
+    "use strict";
+    // Your js script is below this line
+    // --------------------------------------------------------------------- //
+
+    // Example
+    console.log( "ready!" );
+});
