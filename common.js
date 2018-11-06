5c29e3367222bb0669d14af3ac654b378eaf0f32
Inna Shingaryova <i.shingaryova@lovata.com>
2018-11-06 16:21:06 +0300

    Refactor add partial btn-add-to-cart

--- common.js
+++ common.js
@@ -11,6 +11,7 @@ $(() => { // Shorthand for $( document ).ready()
   // Example
   console.log('ready!');
   require('./partials/content/button-change-quantity/button-change-quantity');
+  require('./partials/content/button-add-to-cart/button-add-to-cart');
   require('./partials/product/cart/cart');
   require('./partials/content/form/formValidation');
 });
